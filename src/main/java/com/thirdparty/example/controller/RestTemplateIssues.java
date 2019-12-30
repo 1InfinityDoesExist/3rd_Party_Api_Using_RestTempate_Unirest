c9c0289af917882fa7d37a6d9b3232d47f514ecf
avinash <avinash.patel@kuliza.com>
2019-12-31 02:48:57 +0530

    JSON simple

--- src/main/java/com/thirdparty/example/controller/RestTemplateIssues.java
+++ src/main/java/com/thirdparty/example/controller/RestTemplateIssues.java
@@ -4,13 +4,19 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,9 +26,7 @@ import com.google.gson.JsonObject;
 @RestController
 @RequestMapping(path = "/api/object/rest")
 public class RestTemplateIssues {
-
-	@Autowired
-	private RestTemplate restTemplate;
+	private static final Logger logger = LoggerFactory.getLogger(RestTemplateIssues.class);
 
 	public ResponseEntity<?> saveEmployeeData() {
 		String url = "http://dummy.restapiexample.com/api/v1/create";
@@ -37,6 +41,7 @@ public class RestTemplateIssues {
 		request.put("age", "24");
 
 		HttpEntity<String> httpEntity = new HttpEntity<String>(new Gson().toJson(request), headers);
+		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
 		JsonObject jsonObject = new Gson().fromJson(response.getBody(), JsonObject.class);
 		if (!jsonObject.get("errorMessage").isJsonNull()) {
@@ -44,6 +49,56 @@ public class RestTemplateIssues {
 		}
 		return new ResponseEntity<String>("Successfully Persisited", HttpStatus.CREATED);
 
+	}
+
+	@RequestMapping(path = "/get/rest", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<?> getEmployeeDetails(@RequestParam(value = "id", required = true) String id)
+			throws ParseException {
+
+		final String url = "http://dummy.restapiexample.com/api/v1/employee/";
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity(url + id, String.class);
+
+		JSONParser parser = new JSONParser();
+		JSONObject jsonObject = (JSONObject) parser.parse(response.getBody());
+
+		for (Object object : jsonObject.keySet()) {
+			String propName = (String) object;
+			logger.info("---------->" + propName);
+			switch (propName) {
+			case "id":
+				Object obj1 = jsonObject.get(propName);
+				String value1 = (String) obj1;
+				logger.info("Employee Id: --" + value1);
+				continue;
+			case "employee_name":
+				Object obj2 = jsonObject.get(propName);
+				String str1 = (String) obj2;
+				logger.info("Employee Name:- " + str1);
+				continue;
+			case "employee_salary":
+				Object obj3 = jsonObject.get(propName);
+				String str2 = (String) obj3;
+				logger.info("Employee Salary:- " + str2);
+				continue;
+			case "employee_age":
+				Object obj4 = jsonObject.get(propName);
+				String str3 = (String) obj4;
+				logger.info("Employee Age:- " + str3);
+				continue;
+			case "profile_image":
+				Object obj5 = jsonObject.get(propName);
+				String str4 = (String) obj5;
+				logger.info("Profile Image- " + str4);
+				continue;
+
+			default:
+				logger.info("****************End Of Switch Case***************************");
+			}
+		}
+
+		return new ResponseEntity<String>("SuccessFully Retrieved Data For Employee", HttpStatus.OK);
 	}
 
 }
