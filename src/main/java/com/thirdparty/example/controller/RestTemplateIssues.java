e710eed018e02795fe837663bf6cf08572e1b92e
avinash <avinash.patel@kuliza.com>
2019-12-31 03:09:45 +0530

    post in rest Template done

--- src/main/java/com/thirdparty/example/controller/RestTemplateIssues.java
+++ src/main/java/com/thirdparty/example/controller/RestTemplateIssues.java
@@ -20,15 +20,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 @RestController
 @RequestMapping(path = "/api/object/rest")
 public class RestTemplateIssues {
 	private static final Logger logger = LoggerFactory.getLogger(RestTemplateIssues.class);
 
-	public ResponseEntity<?> saveEmployeeData() {
+	@RequestMapping(path = "/create", method = RequestMethod.POST)
+	public ResponseEntity<?> saveEmployeeData() throws ParseException, JsonMappingException, JsonProcessingException {
 		String url = "http://dummy.restapiexample.com/api/v1/create";
 
 		HttpHeaders headers = new HttpHeaders();
@@ -36,32 +39,76 @@ public class RestTemplateIssues {
 		headers.setContentType(MediaType.APPLICATION_JSON);
 
 		Map<String, String> request = new HashMap<String, String>();
-		request.put("name", "Avinash Patel");
+		request.put("name", "Avinash Rocks");
 		request.put("salary", "600000");
 		request.put("age", "24");
 
 		HttpEntity<String> httpEntity = new HttpEntity<String>(new Gson().toJson(request), headers);
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-		JsonObject jsonObject = new Gson().fromJson(response.getBody(), JsonObject.class);
-		if (!jsonObject.get("errorMessage").isJsonNull()) {
-			return new ResponseEntity<String>("Something went Wrong!!!", HttpStatus.BAD_REQUEST);
-		}
-		return new ResponseEntity<String>("Successfully Persisited", HttpStatus.CREATED);
+		ResponseEntity<String> response = restTemplate.postForEntity(url, httpEntity, String.class);
+		// One Way Of Parsing
+		JSONParser parser = new JSONParser();
+		JSONObject jsonObject = (JSONObject) parser.parse(response.getBody());
+		logger.info("jsonObject:- " + jsonObject);
 
+		// Another Way Of Parsing
+		JSONObject jObject = new ObjectMapper().readValue(response.getBody(), JSONObject.class);
+		logger.info("objectMapper:-" + jObject);
+
+		for (Object object : jsonObject.keySet()) {
+			String propName = (String) object;
+			logger.info("---------->" + propName);
+			switch (propName) {
+			case "id":
+				Object obj1 = jsonObject.get(propName);
+				String value1 = (String) obj1;
+				logger.info("Employee Id: --" + value1);
+				continue;
+			case "employee_name":
+				Object obj2 = jsonObject.get(propName);
+				String str1 = (String) obj2;
+				logger.info("Employee Name:- " + str1);
+				continue;
+			case "employee_salary":
+				Object obj3 = jsonObject.get(propName);
+				String str2 = (String) obj3;
+				logger.info("Employee Salary:- " + str2);
+				continue;
+			case "employee_age":
+				Object obj4 = jsonObject.get(propName);
+				String str3 = (String) obj4;
+				logger.info("Employee Age:- " + str3);
+				continue;
+			case "profile_image":
+				Object obj5 = jsonObject.get(propName);
+				String str4 = (String) obj5;
+				logger.info("Profile Image- " + str4);
+				continue;
+
+			default:
+				logger.info("****************End Of Switch Case***************************");
+			}
+		}
+
+		return new ResponseEntity<String>("SuccessFully Retrieved Data For Employee", HttpStatus.OK);
 	}
 
 	@RequestMapping(path = "/get/rest", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<?> getEmployeeDetails(@RequestParam(value = "id", required = true) String id)
-			throws ParseException {
+			throws ParseException, JsonMappingException, JsonProcessingException {
 
 		final String url = "http://dummy.restapiexample.com/api/v1/employee/";
 
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<String> response = restTemplate.getForEntity(url + id, String.class);
 
+		// One Way Of Parsing
 		JSONParser parser = new JSONParser();
 		JSONObject jsonObject = (JSONObject) parser.parse(response.getBody());
+
+		// Another Way Of Parsing
+		JSONObject jObject = new ObjectMapper().readValue(response.getBody(), JSONObject.class);
+		logger.info("objectMapper:-" + jObject);
 
 		for (Object object : jsonObject.keySet()) {
 			String propName = (String) object;
